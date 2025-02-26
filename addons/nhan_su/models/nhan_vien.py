4f39ed4c9df6be8d4309d6f760b29872645b0230
Phuphu2310 <thieukhanhphu2310@gmail.com>
2025-02-26 09:17:13 +0700

    first commit

--- addons/nhan_su/models/nhan_vien.py
+++ addons/nhan_su/models/nhan_vien.py
@@ -4,9 +4,13 @@ from odoo import models, fields, api
 class NhanVien(models.Model):
     _name = 'nhan_vien'
     _description = 'Bảng chứa thông tin nhân viên'
+    _rec_name = 'ho_va_ten'
 
     ma_dinh_danh = fields.Char("Mã định danh", required=True)
     ngay_sinh = fields.Date("Ngày sinh")
     que_quan = fields.Char("Quê quán")
     email = fields.Char("Email")
     so_dien_thoai = fields.Char("Số điện thoại")
+    ho_va_ten = fields.Char("Họ và tên")
+    phong_ban = fields.Char("Phòng ban")
+
---
+++ addons/quan_ly_phong_hop/models/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+from . import quan_ly_phong_hop
+
